bd075a64f7081a437caee0fb30877fd3f6532cee
Aipiro <1535417168@qq.com>
2022-09-19 18:56:57 +1200

    Create

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,24 @@
-import { Navigate, Route, Routes } from "react-router-dom";
-import CheckoutPage from "./CheckoutPage";
-import OrdersPage from "./OrdersPage";
-import PageLayout from "./PageLayout";
-import ShopPage from "./ShopPage";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-
   return (
-    <Routes>
-      <Route path="/" element={<PageLayout />}>
-
-        <Route index element={<Navigate to="shop" replace />} />
-
-        <Route path="shop" element={<ShopPage /> } />
-        <Route path="checkout" element={<CheckoutPage /> } />
-        <Route path="history" element={<OrdersPage /> } />
-      </Route>
-    </Routes>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
