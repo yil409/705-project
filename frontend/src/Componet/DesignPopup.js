e0a5750685c6f488c3b088b9797d427fd31431af
Xinyu Zhu <zhuxinyu1997@gmail.com>
2022-10-05 16:22:04 +1300

    DesignPopup Page

--- frontend/src/Componet/DesignPopup.js
+++ frontend/src/Componet/DesignPopup.js
@@ -1,16 +1,123 @@
-import React from 'react';
-import { Link } from "react-router-dom";
+import React from 'react';  
+import Box from "@mui/material/Box";
+
+import { Link, renderMatches } from "react-router-dom";
+
+import FileSelection from './design/DesignFileSelection';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { purple } from '@mui/material/colors';
+import { DataGrid } from '@mui/x-data-grid';
+
 import '.././App.css';
+
+import { styled } from '@mui/material/styles';
+
+
+
+
 
 
 
 export default function DesignPopup() {
-    return (
-        <>
 
-            <h1> This is the popup modal window's designing page</h1>
-            <div className="nextpageButton"> <Link to="/test" style={{color: "white"}}>NEXTPAGE</Link> </div>
+
+    const columns = [
+        { 
+            field: 'id', headerName: 'ID', width: 90 
+        },
+        {
+            field: 'fileName',
+            headerName: 'File name',
+            width: 550,
+        },
+        {
+            field: 'dateModified',
+            headerName: 'Date modified',
+            width: 160,
+        },
+      ];
+      
+      const rows = [
+        { id: 1, fileName: 'COMPSCI 705 Assignment', dateModified: '04-10-2022' },
+        { id: 2, fileName: 'COMPSCI 705 Slides', dateModified: '01-10-2022' },
+        { id: 3, fileName: 'COMPSCI 705 Recording', dateModified: '05-09-2022'},
+        { id: 4, fileName: 'COMPSCI 760 Assignment', dateModified: '04-08-2022' },
+      ];
+
+      function handleSubmit(e) {
+        e.preventDefault();
+        console.log('You clicked submit.');
+      }
+    
+
+    
+
+    return (
+        <div>
+            <div
+                className="head"
+                style={{
+                    width: "fit-content",
+                    margin: "auto",
+                }}
+            >
+                <h1
+                    style={{
+                        color: "blue",
+                    }}
+                >
+                    This is the popup modal window's designing page!
+                </h1>
+                <strong>Choose a file below to upload</strong>
+            </div>
+            <br />
+            <center>
+                <Box
+                    sx={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                        margin: "auto",
+                        width: "fit-content",
+                        "& > :not(style)": {
+                            m: 1,
+                            width: 900,
+                            height: 300,
+                        },
+                    }}
+                >
+
+
+
+                    <DataGrid autoHeight
+                        rows={rows}
+                        columns={columns}
+                        // checkboxSelection
+                    />
+
+                    
+
+
+
+
+                </Box>
+                <Box
+                    sx={{
+                        // float: left,
+                        width: 0.8,
+                        height: 25,
+                        border: 1 ,
+                        padding: 20,
+                    }}
+                // className="fileUploadArea"
+                >
+                    <FileSelection onSubmit={handleSubmit}/>           
+                </Box>
+            </center>
             
-        </>
-    )
+            <div className="nextpageButton"> <Link to="/test" style={{color: "white"}}>NEXTPAGE</Link> </div>
+        </div>
+        
+    );
+    
 }
+
--- frontend/src/Componet/design/DesignFileSelection.js
+++ frontend/src/Componet/design/DesignFileSelection.js
@@ -1,5 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import { Rnd }  from 'react-rnd';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import Dialog from '@mui/material/Dialog';
+
+
+import Button from '@mui/material/Button';
+
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
 
 const style = {
     display: "flex",
@@ -10,6 +23,18 @@ const style = {
     color: "#fff",
     borderRadius: "6px",
     cursor: "pointer",
+  };
+
+  const style_modal = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
   };
 
   function FileSelection () {
@@ -26,15 +51,126 @@ const style = {
       localStorage.setItem("confirmButton", JSON.stringify(confirmInfo));
     }, [confirmInfo]);
 
+    function myFunction(){
+      console.log("DDSS")
+    }
+
+    const [drag, setDrag] = useState(false);
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+
+    // const eventControl = (event) => {
+
+
+    //   if (event.type === 'mousemove' || event.type === 'touchmove') {
+    //     setDrag(true)
+    //   }
+  
+    //   if (event.type === 'mouseup' || event.type === 'touchend') {
+    //     setTimeout(() => {
+    //       setDrag(false);
+    //     }, 100);
+  
+    //   }
+    // }
+
+    // const handleClick = () =>
+    // Dialog({
+    //   // set the dialog's title
+    //   title: "There's change in the Air!",
+    //   // include some text to show the user, NOTE: this could be any arbitrary
+    //   // component, not just a string.
+    //   contentText: "Here's what's new in version 2.0 ...",
+    //   // don't render the cancel button, because in this case the only thing a
+    //   // user can do is "dismiss" the notification.
+    //   cancelButton: false,
+    //   // Mui defaults to text buttons, let's use a contained one styled with
+    //   // the theme's primary color
+    //   submitButton: {
+    //     children: "Dismiss",
+    //     props: {
+    //       variant: "contained",
+    //       color: "primary"
+    //     }
+    //   }});
+
     
       return (
 
+        // <Rnd
+          
+        //   // onDrag={()=>setDrag(true)}
+        //   // onDragStart={() => {
+        //   //   setDrag(true)
+        //   // }}
+
+        //   style={style}
+        //   size={{ width: confirmInfo.width, height: confirmInfo.height }}
+        //   position={{ x: confirmInfo.x, y: confirmInfo.y }}
+        //   onDragStop={(e, d) => {
+
+        //     // eventControl()
+
+        //     // setDrag(false)
+        //     // if(setDrag==true){
+        //       setConfirmInfo({...confirmInfo, x: d.x, y: d.y });
+        //       console.log(setDrag)
+        //     // }
+        //     // setConfirmInfo({...confirmInfo, x: d.x, y: d.y });
+
+            
+
+        //   }}
+        //   onResizeStop={(e, direction, ref, delta, position) => {
+        //     setConfirmInfo({
+        //       width: ref.style.width,
+        //       height: ref.style.height,
+        //       ...position
+        //     });
+        //   }}
+          
+        //   bounds="parent"
+        //   // onclick={handleClose}
+        //   // onDoubleClick={handleClick}
+        //   // onClick={()=>{
+        //   //   if(drag){
+        //   //     return
+        //   //   }
+        //   //   setOpen(true)}
+        //   // }
+        // >
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         <Rnd
+
+          onDrag={()=>setDrag(true)}
+
           style={style}
           size={{ width: confirmInfo.width, height: confirmInfo.height }}
           position={{ x: confirmInfo.x, y: confirmInfo.y }}
           onDragStop={(e, d) => {
             setConfirmInfo({...confirmInfo, x: d.x, y: d.y });
+
+            setDrag(false)
           }}
           onResizeStop={(e, direction, ref, delta, position) => {
             setConfirmInfo({
@@ -45,7 +181,47 @@ const style = {
           }}
           bounds="parent"
         >
+          <div 
+          // style={{height:"100vh",width:"100vh"}} 
+          // onClick={setOpen(true)}
+          onClick={()=>{if(drag){return}
+           handleClickOpen()}}
+          >
           Confirm
+          </div>
+
+          <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-describedby="alert-dialog-description"
+          >
+            
+            <DialogContent>
+              <DialogContentText id="alert-dialog-description">
+                Please confirm your submission
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>NO</Button>
+              <Button onClick={handleClose} autoFocus>
+                YES
+              </Button>
+            </DialogActions>
+          </Dialog>
+
+
+
+          {/* <Modal
+            open={open}
+            onClose={()=>setOpen(false)}
+            aria-labelledby="modal-modal-title"
+          >
+            <Box sx={style_modal}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                You have upload the file successfully
+              </Typography>
+            </Box>
+          </Modal>   */}
         </Rnd>
         
       );
